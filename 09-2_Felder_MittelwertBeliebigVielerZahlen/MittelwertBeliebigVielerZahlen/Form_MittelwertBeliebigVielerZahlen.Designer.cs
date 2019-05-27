5a715de1611f4761aae9f1c55f3981f8b9dba4dd
Horst-Christian <50948929+horstH2@users.noreply.github.com>
2019-05-27 14:53:00 +0200

    Dynamisierung während der Laufzeit

---
+++ 09-2_Felder_MittelwertBeliebigVielerZahlen/MittelwertBeliebigVielerZahlen/Form_MittelwertBeliebigVielerZahlen.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MittelwertBeliebigVielerZahlen
+{
+  partial class Form_MittelwertBeliebigVielerZahlen
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBox_Eingabewert = new System.Windows.Forms.TextBox();
+      this.textBox_Mittelwert = new System.Windows.Forms.TextBox();
+      this.textBox_Anzahl = new System.Windows.Forms.TextBox();
+      this.label_Eingabewert = new System.Windows.Forms.Label();
+      this.label_Mittelwert = new System.Windows.Forms.Label();
+      this.label_Anzahl = new System.Windows.Forms.Label();
+      this.button_Uebernahme = new System.Windows.Forms.Button();
+      this.menuStrip_Mittelwert = new System.Windows.Forms.MenuStrip();
+      this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.menuStrip_Mittelwert.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textBox_Eingabewert
+      // 
+      this.textBox_Eingabewert.Location = new System.Drawing.Point(284, 87);
+      this.textBox_Eingabewert.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+      this.textBox_Eingabewert.Name = "textBox_Eingabewert";
+      this.textBox_Eingabewert.Size = new System.Drawing.Size(260, 44);
+      this.textBox_Eingabewert.TabIndex = 0;
+      this.textBox_Eingabewert.Text = "0";
+      // 
+      // textBox_Mittelwert
+      // 
+      this.textBox_Mittelwert.Location = new System.Drawing.Point(284, 236);
+      this.textBox_Mittelwert.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+      this.textBox_Mittelwert.Name = "textBox_Mittelwert";
+      this.textBox_Mittelwert.Size = new System.Drawing.Size(260, 44);
+      this.textBox_Mittelwert.TabIndex = 1;
+      this.textBox_Mittelwert.Text = "0";
+      // 
+      // textBox_Anzahl
+      // 
+      this.textBox_Anzahl.Location = new System.Drawing.Point(284, 310);
+      this.textBox_Anzahl.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+      this.textBox_Anzahl.Name = "textBox_Anzahl";
+      this.textBox_Anzahl.Size = new System.Drawing.Size(260, 44);
+      this.textBox_Anzahl.TabIndex = 2;
+      this.textBox_Anzahl.Text = "0";
+      // 
+      // label_Eingabewert
+      // 
+      this.label_Eingabewert.AutoSize = true;
+      this.label_Eingabewert.Location = new System.Drawing.Point(56, 94);
+      this.label_Eingabewert.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label_Eingabewert.Name = "label_Eingabewert";
+      this.label_Eingabewert.Size = new System.Drawing.Size(194, 37);
+      this.label_Eingabewert.TabIndex = 3;
+      this.label_Eingabewert.Text = "Eingabewert";
+      // 
+      // label_Mittelwert
+      // 
+      this.label_Mittelwert.AutoSize = true;
+      this.label_Mittelwert.Location = new System.Drawing.Point(56, 244);
+      this.label_Mittelwert.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label_Mittelwert.Name = "label_Mittelwert";
+      this.label_Mittelwert.Size = new System.Drawing.Size(152, 37);
+      this.label_Mittelwert.TabIndex = 4;
+      this.label_Mittelwert.Text = "Mittelwert";
+      // 
+      // label_Anzahl
+      // 
+      this.label_Anzahl.AutoSize = true;
+      this.label_Anzahl.Location = new System.Drawing.Point(56, 318);
+      this.label_Anzahl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.label_Anzahl.Name = "label_Anzahl";
+      this.label_Anzahl.Size = new System.Drawing.Size(116, 37);
+      this.label_Anzahl.TabIndex = 5;
+      this.label_Anzahl.Text = "Anzahl";
+      // 
+      // button_Uebernahme
+      // 
+      this.button_Uebernahme.Location = new System.Drawing.Point(62, 157);
+      this.button_Uebernahme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.button_Uebernahme.Name = "button_Uebernahme";
+      this.button_Uebernahme.Size = new System.Drawing.Size(483, 57);
+      this.button_Uebernahme.TabIndex = 6;
+      this.button_Uebernahme.Text = "Übernahme";
+      this.button_Uebernahme.UseVisualStyleBackColor = true;
+      this.button_Uebernahme.Click += new System.EventHandler(this.button_Uebernahme_Click);
+      // 
+      // menuStrip_Mittelwert
+      // 
+      this.menuStrip_Mittelwert.ImageScalingSize = new System.Drawing.Size(24, 24);
+      this.menuStrip_Mittelwert.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beendenToolStripMenuItem});
+      this.menuStrip_Mittelwert.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip_Mittelwert.Name = "menuStrip_Mittelwert";
+      this.menuStrip_Mittelwert.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+      this.menuStrip_Mittelwert.Size = new System.Drawing.Size(604, 55);
+      this.menuStrip_Mittelwert.TabIndex = 7;
+      this.menuStrip_Mittelwert.Text = "menuStrip1";
+      // 
+      // beendenToolStripMenuItem
+      // 
+      this.beendenToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+      this.beendenToolStripMenuItem.Size = new System.Drawing.Size(156, 49);
+      this.beendenToolStripMenuItem.Text = "Beenden";
+      this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+      // 
+      // Form_MittelwertBeliebigVielerZahlen
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(604, 390);
+      this.Controls.Add(this.button_Uebernahme);
+      this.Controls.Add(this.label_Anzahl);
+      this.Controls.Add(this.label_Mittelwert);
+      this.Controls.Add(this.label_Eingabewert);
+      this.Controls.Add(this.textBox_Anzahl);
+      this.Controls.Add(this.textBox_Mittelwert);
+      this.Controls.Add(this.textBox_Eingabewert);
+      this.Controls.Add(this.menuStrip_Mittelwert);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.MainMenuStrip = this.menuStrip_Mittelwert;
+      this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+      this.Name = "Form_MittelwertBeliebigVielerZahlen";
+      this.Text = "Mittelwert beliebig vieler Zahlen";
+      this.menuStrip_Mittelwert.ResumeLayout(false);
+      this.menuStrip_Mittelwert.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox_Eingabewert;
+    private System.Windows.Forms.TextBox textBox_Mittelwert;
+    private System.Windows.Forms.TextBox textBox_Anzahl;
+    private System.Windows.Forms.Label label_Eingabewert;
+    private System.Windows.Forms.Label label_Mittelwert;
+    private System.Windows.Forms.Label label_Anzahl;
+    private System.Windows.Forms.Button button_Uebernahme;
+    private System.Windows.Forms.MenuStrip menuStrip_Mittelwert;
+    private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+  }
+}
+
---
+++ 09-2_Felder_MittelwertBeliebigVielerZahlen/MittelwertBeliebigVielerZahlen/Form_MittelwertBeliebigVielerZahlen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MittelwertBeliebigVielerZahlen
+{
+  public partial class Form_MittelwertBeliebigVielerZahlen : Form
+  {
+    public Form_MittelwertBeliebigVielerZahlen()
+    {
+      InitializeComponent();
+    }
+
+/// <summary>
+///  Feld und zwischensumme müssen global vereinbart werden
+/// </summary>
+// Ein Feld mit keinem Element
+double[] werte = new double[0];
+// Summe für Mittelwertberechnung
+double summe = 0;
+/// <summary>
+/// Berechnet Button-Klick kumulativ den Mittelwert.
+/// Das Feld wird jeweils um ein Element erweitert.
+/// Sämtliche Eingabewerte sind dortabgespeichert.
+/// </summary>
+private void button_Uebernahme_Click(object sender, EventArgs e)
+{
+  // Ein Element wird an das Feld angehängt
+  Array.Resize(ref werte, werte.Length + 1);
+  // Wert wird per Formulareingabe übernommen
+  werte[werte.Length - 1] = Convert.ToDouble(textBox_Eingabewert.Text);
+  // Summe wird kumulativ berechnet
+  summe += werte[werte.Length - 1];
+  //Mittelwertberechnung und Ausgabe
+  textBox_Mittelwert.Text = (summe / werte.Length).ToString();
+  // Augabe der Anzahl der Feldelemente
+  textBox_Anzahl.Text = werte.Length.ToString();
+}
+
+private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  Close();
+}
+  }
+}
