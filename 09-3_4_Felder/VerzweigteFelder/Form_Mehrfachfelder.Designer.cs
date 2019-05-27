ecdd265f7cb0ed329deb25b469ce6355762b04a5
Horst-Christian <50948929+horstH2@users.noreply.github.com>
2019-05-27 14:55:16 +0200

    Verzweigte Felder und Strukturen

---
+++ 09-3_4_Felder/VerzweigteFelder/Form_Mehrfachfelder.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace VerzweigteFelder
+{
+  partial class Form_VerzweigtesFeld
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.erzeugeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox_Feld = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.erzeugeToolStripMenuItem,
+            this.beendenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(21, 6, 0, 6);
+            this.menuStrip1.Size = new System.Drawing.Size(1340, 102);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Mehrfachfelder";
+            // 
+            // erzeugeToolStripMenuItem
+            // 
+            this.erzeugeToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.erzeugeToolStripMenuItem.Name = "erzeugeToolStripMenuItem";
+            this.erzeugeToolStripMenuItem.Size = new System.Drawing.Size(272, 90);
+            this.erzeugeToolStripMenuItem.Text = "Erzeuge";
+            this.erzeugeToolStripMenuItem.Click += new System.EventHandler(this.erzeugeToolStripMenuItem_Click);
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(295, 90);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // textBox_Feld
+            // 
+            this.textBox_Feld.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Feld.Location = new System.Drawing.Point(0, 102);
+            this.textBox_Feld.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox_Feld.Multiline = true;
+            this.textBox_Feld.Name = "textBox_Feld";
+            this.textBox_Feld.Size = new System.Drawing.Size(1340, 526);
+            this.textBox_Feld.TabIndex = 1;
+            // 
+            // Form_VerzweigtesFeld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(37F, 73F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1340, 628);
+            this.Controls.Add(this.textBox_Feld);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form_VerzweigtesFeld";
+            this.Text = "Verzweigtes Feld";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem erzeugeToolStripMenuItem;
+    private System.Windows.Forms.TextBox textBox_Feld;
+  }
+}
+
---
+++ 09-3_4_Felder/VerzweigteFelder/Form_Mehrfachfelder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerzweigteFelder
+{
+  public partial class Form_VerzweigtesFeld : Form
+  {
+    public Form_VerzweigtesFeld()
+    {
+      InitializeComponent();
+    }
+    private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+/// <summary>
+/// Erzeugt ein zweidimensionales verzwigtes Feld und
+/// füllt es mit Zufallsdaten
+/// </summary>
+private void erzeugeToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  // Mehrfachfeld mit 5 Zeilen
+  double[][] feld = new double[5][];
+  // Jede Zeile erhält seine individulle Elementeanzahl
+  Random spalenanzahl = new Random();
+  for (int zeile = 0; zeile < 5; zeile++)
+  {
+    //Eine Zeile mit zufällig 1 bis maximal 6 Elementen
+    feld[zeile] = new double[spalenanzahl.Next(1, 7)];
+  }
+  //Alle Feldelemente sollen jetzt mit Zufallswerten gefüllt werden
+  Random zufallswert = new Random(3);
+  for (int zeile = 0; zeile < feld.Length; zeile++)
+  {
+    for (int spalte = 0; spalte < feld[zeile].Length; spalte++)
+    {
+      // Zufallsewerte liegen zwischen 0 und exclusiv 100
+      feld[zeile][spalte] = zufallswert.Next(0, 100);
+    }
+  }
+  // Ausgabe aller Feldelemente in einer Textbox
+  for (int zeile = 0; zeile < feld.Length; zeile++)
+  {
+    textBox_Feld.Text += "\r\n"; //Neue Zeile
+    for (int spalte = 0; spalte < feld[zeile].Length; spalte++)
+    {
+      // Elemente einer Zeile
+      textBox_Feld.Text += feld[zeile][spalte].ToString("00") +", ";
+    }
+  }
+    }
+  }
+}
